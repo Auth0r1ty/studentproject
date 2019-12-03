a55cf211ef82fb530374221f737ce819f2ec54d2
Auth0r1ty <58190951+Auth0r1ty@users.noreply.github.com>
2019-12-03 14:13:45 +0100

    Main loop

---
+++ CubChase/Play.py
@@ -0,0 +1,28 @@
+import pygame
+pygame.init()
+
+carryOn = True      #promenljiva koja drzi loop aktivnim
+
+#clock nam sluzi da kaze kojom brzinom ce se igra update-ovati
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            carryOn = False  # Flag that we are done so we exit this loop
+
+        # --- Game logic should go here
+
+        # --- Drawing code should go here
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+
