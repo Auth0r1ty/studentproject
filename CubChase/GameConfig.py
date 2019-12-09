3a4057038cab706b9ac8f61395bb8ebf500a4d19
Vukasin <vule993@outlook.com>
2019-12-09 15:21:29 +0100

    config fajl
    u njemu cemo definisati ono sto se definise jednom -> ucitati slike, napraviti promenljive, matricu itd, metoda map_init kreira mapu i ogranicava je na velicinu prozora

---
+++ CubChase/GameConfig.py
@@ -0,0 +1,118 @@
+from enum import Enum
+import pygame
+from GameStaticObject import GameStaticObject
+from Player import Player
+
+#images
+# LOAD NECESSARY IMAGES
+# define wall image
+wall = pygame.image.load("img/wall.png")
+wall = pygame.transform.scale(wall, (50, 50))
+# define path image
+path = pygame.image.load("img/sand_empty.png")
+path = pygame.transform.scale(path, (50, 50))
+# define player image
+timon = pygame.image.load("img/timon.png")
+timon = pygame.transform.scale(timon, (50, 50))
+#enter image
+enter = pygame.image.load("img/sapice_red.png")
+enter = pygame.transform.scale(enter, (50, 50))
+
+#constants
+speed = 5
+sprite_list = pygame.sprite.Group()
+player = Player(timon, 50, 50, 250, 400)
+player1 = Player(timon, 50, 50, 250, 350)
+
+sprite_list.add(player)
+sprite_list.add(player1)
+player.sprite_list = sprite_list
+player1.sprite_list = sprite_list
+
+#enums
+class StaticEl(Enum):
+    path = 1
+    wall = 2
+    enter = 3
+    trap = 4
+    pathPlayer1 = 5   #
+    pathPlayer2 = 6
+    pathPlayer3 = 7
+    pathPlayer4 = 8
+
+
+#map matrix
+gameMap = (
+        # 1 (9)             2 (10)           3 (11)       4 (12)        5(13)           6(14)         7(15)          8(16)
+        (StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path),
+
+        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
+         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
+
+        (StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.enter, StaticEl.wall, StaticEl.path, StaticEl.path),
+
+        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path,
+         StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path),
+
+        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path,
+         StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path),
+
+        (StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
+
+        (StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
+         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
+
+        (StaticEl.path, StaticEl.wall, StaticEl.enter, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path),
+        #9 red je ovaj ispod
+        (StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
+         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path),
+        #10 red je ovaj ispod
+        (StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
+
+        (StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
+         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall),
+
+        (StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
+         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.enter),
+
+    )
+
+
+def map_init():
+    for i in range(0, 12):
+        for j in range(0, 16):
+            if gameMap[i][j] == StaticEl.wall:
+                obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
+                sprite_list.add(obj)
+    i = -1
+    for j in range(0, 16):
+        obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
+        sprite_list.add(obj)
+    i = 12
+    for j in range(0, 16):
+        obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
+        sprite_list.add(obj)
+    j = -1
+    for i in range(0, 12):
+        obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
+        sprite_list.add(obj)
+    j = 16
+    for i in range(0, 12):
+        obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
+        sprite_list.add(obj)
+
+
+
+
+'''
+
+class GameConfig():
+    def __init__(self):
+        pass
+
+'''
--- CubChase/Play.py
+++ CubChase/Play.py
@@ -1,27 +1,7 @@
 import pygame
-from enum import Enum
-from Player import Player
-from GameStaticObject import GameStaticObject
+import GameConfig as config
 
 pygame.init()
-
-
-########################################################################################################
-
-
-
-
-# elementi koji se ne krecu na mapi da pratimo na mapi (preko enumeracije)
-class StaticEl(Enum):
-    path = 1
-    wall = 2
-    enter = 3
-    trap = 4
-    endmap = 5
-    pathPlayer1 = 6   #
-    pathPlayer2 = 7
-    pathPlayer3 = 8
-    pathPlayer4 = 9
 
 
 size = (800, 600)
@@ -34,104 +14,17 @@ carryOn = True
 
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 
 
-# LOAD NECESSARY IMAGES
-# define wall image
-wall = pygame.image.load("img/wall.png")
-wall = pygame.transform.scale(wall, (50, 50))
-# define path image
-path = pygame.image.load("img/sand_empty.png")
-path = pygame.transform.scale(path, (50, 50))
-# define player image
-timon = pygame.image.load("img/timon.png")
-timon = pygame.transform.scale(timon, (50, 50))
-#enter image
-enter = pygame.image.load("img/sapice_red.png")
-enter = pygame.transform.scale(enter, (50, 50))
-
-
-
-
-
-gameMap = (
-        # 1 (9)             2 (10)           3 (11)       4 (12)        5(13)           6(14)         7(15)          8(16)
-        (StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path),
-
-        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
-         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
-
-        (StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.enter, StaticEl.wall, StaticEl.path, StaticEl.path),
-
-        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path,
-         StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path),
-
-        (StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path,
-         StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path),
-
-        (StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
-
-        (StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
-         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
-
-        (StaticEl.path, StaticEl.wall, StaticEl.enter, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path),
-        #9 red je ovaj ispod
-        (StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
-         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.path),
-        #10 red je ovaj ispod
-        (StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path),
-
-        (StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path,
-         StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall),
-
-        (StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.wall, StaticEl.wall, StaticEl.wall,
-         StaticEl.wall, StaticEl.wall, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.path, StaticEl.enter),
-
-    )
+config.map_init()
 
 ########################################################################################################
 
 # sprites_list sadrzi sve sprit-ove napravljene u igrici
-sprite_list = pygame.sprite.Group()
-
-for i in range(0, 12):
-    for j in range(0, 16):
-        if gameMap[i][j] == StaticEl.wall:
-            obj = GameStaticObject(wall, 50, 50, j*50, i*50)
-            sprite_list.add(obj)
-i = -1
-for j in range(0, 16):
-    obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
-    sprite_list.add(obj)
-i = 12
-for j in range(0, 16):
-    obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
-    sprite_list.add(obj)
-j = -1
-for i in range(0, 12):
-    obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
-    sprite_list.add(obj)
-j = 16
-for i in range(0, 12):
-    obj = GameStaticObject(wall, 50, 50, j * 50, i * 50)
-    sprite_list.add(obj)
-
-# napravi se objekat tipa Player
-player = Player(timon, 50, 50, 250, 400)
-player1 = Player(timon, 50, 50, 250, 350)
 
 
-sprite_list.add(player)
-sprite_list.add(player1)
 
-player.sprite_list = sprite_list
-player1.sprite_list = sprite_list
 
 
 
@@ -152,33 +45,33 @@ while carryOn:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player.movePlayer(-2, 0, sprite_list)
+        config.player.movePlayer(-config.speed, 0, config.sprite_list)
         # player.moveLeft (2, timon)
     if keys[pygame.K_RIGHT]:
-        player.movePlayer (2, 0, sprite_list)
+        config.player.movePlayer(config.speed, 0, config.sprite_list)
         # player.moveRight (2, timon)
     if keys[pygame.K_UP]:
-        player.movePlayer (0, -2, sprite_list)
+        config.player.movePlayer (0, -config.speed, config.sprite_list)
         # player.moveUp (2, timon)
     if keys[pygame.K_DOWN]:
-        player.movePlayer (0, 2, sprite_list)
+        config.player.movePlayer (0, config.speed, config.sprite_list)
         # player.moveDown (2, timon)
 
-    sprite_list.update()
+    config.sprite_list.update()
     screen.fill(WHITE)
 
     #iscrtavanje mape
     for i in range(0, 12):
         for j in range(0, 16):
-            if(gameMap[i][j] == StaticEl.path):
-                screen.blit(path, (j*50, i*50))
-            elif (gameMap[i][j] == StaticEl.wall):
-                screen.blit(wall, (j * 50, i * 50))
-            elif (gameMap[i][j] == StaticEl.enter):
-                screen.blit(enter, (j * 50, i * 50))
+            if config.gameMap[i][j] == config.StaticEl.path:
+                screen.blit(config.path, (j*50, i*50))
+            elif config.gameMap[i][j] == config.StaticEl.wall:
+                screen.blit(config.wall, (j * 50, i * 50))
+            elif config.gameMap[i][j] == config.StaticEl.enter:
+                screen.blit(config.enter, (j * 50, i * 50))
     # The you can draw different shapes and lines or add text to your background stage.
 
-    sprite_list.draw(screen)
+    config.sprite_list.draw(screen)
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
